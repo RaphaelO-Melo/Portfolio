5f4e6f55202a8efdce218f132f72e1838bcbfbaa
RaphaelO-Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2020-08-10 16:13:31 -0300

    Skill lvls created with javascript

--- js/page_constructor.js
+++ js/page_constructor.js
@@ -14,9 +14,57 @@ function createHeaderLinks(){
                 ${REFERENCES_TXT[cont]}
             </a>
         </li>
-       `
+       `;
     }
 }
 
 
+function createSkillsLvl(){
+    const REFERENCES_SKILL_NAMES = ['HTML5', 'CSS3', 'JAVASCRIPT (ES6)', 'BOOTSTRAP 4', 'PHP 7', 'MySQL'];
+    const REFERENCES_SKILL_LEVELS = [4, 3, 3, 3, 2, 2];
+    let resp_string = '';
+
+    for(cont = 0; cont < REFERENCES_SKILL_NAMES.length; cont++){
+        resp_string += 
+        `<div class="col-10 ml-auto mr-auto skill-area clearfix pb-3">
+            <h6>${REFERENCES_SKILL_NAMES[cont]}</h6>
+        `;
+        for(cont_2 = 0; cont_2 < 5; cont_2++){
+            if(cont_2 < REFERENCES_SKILL_LEVELS[cont]){
+                resp_string += `<div class="float-left skill-lvl-full"></div>`
+            } else {
+                resp_string += `<div class="float-left skill-lvl-empty"></div>`
+            }
+            
+        }
+        resp_string += `</div>`;
+    }
+    
+
+    document.getElementById('skill-container').innerHTML += resp_string;
+}
+
+function createProjects(){
+    const REFERENCES_PROJECTS = 
+    [
+        {
+            name: 10,
+            descricao: 'não sei'
+        },
+
+        {
+            name: 15,
+            descricao: 'não sei 02'
+        }
+
+    ]
+
+    console.log(REFERENCES_PROJECTS);
+}
+
+
+
+//Criação dos elementos
 createHeaderLinks();
+createSkillsLvl();
+createProjects();
